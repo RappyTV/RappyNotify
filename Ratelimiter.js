8346079c9ab55d5ad9400152cd7e36742ac4fdb7
RappyTV <rappytv@rappytv.com>
2023-08-17 20:18:54 +0200

    Implement basic functionality

---
+++ Ratelimiter.js
@@ -0,0 +1,73 @@
+
+/**
+ * @singleton
+ * @class Ratelimiter
+ * @description This class is used to store the rate limit information
+ */
+
+class Ratelimiter {
+    /**
+     * @type {Map<string, { requests: number, timestamp: number }>}
+     */
+    ips = new Map();
+    maxRequests = server.cfg.ratelimiter.max;
+    timeWindow = server.cfg.ratelimiter.seconds * 1000;
+
+    constructor() {
+        this.ips = new Map();
+        this.maxRequests = server.cfg.ratelimiter.max;
+        this.timeWindow = server.cfg.ratelimiter.seconds * 1000;
+    }
+
+    /**
+     * 
+     * @param {Request} req 
+     * @param {Response} res 
+     * @returns 
+     */
+
+    ratelimitResponse(req, res) {
+        if(!server.cfg.ratelimiter.enabled) return false;
+        const ratelimitData = this.getRateLimitInfo(req.ip);
+        res.setHeader(`X-RateLimit-Limit`, this.maxRequests);
+        if(!ratelimitData.limited) res.setHeader(`X-RateLimit-Remaining`, ratelimitData.remaining); // ik this looks weird but the order of the headers is important
+        res.setHeader(`X-RateLimit-Reset`, ratelimitData.reset / 1000);
+        if(ratelimitData.limited) {
+            res.status(429).send({ error: `You're being ratelimited! Please try again in ${Math.ceil(ratelimitData.reset / 1000)} seconds!` });
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param {string} ip 
+     * @returns {{ reset: number } & ({ limited: true } | { limited: false, remaining: number })}
+     */
+
+    getRateLimitInfo(ip) {
+        const player = this.ips.get(ip);
+        if(!player) {
+            this.ips.set(ip, {
+                requests: 1,
+                timestamp: Date.now()
+            });
+            return {
+                remaining: this.maxRequests - 1,
+                reset: this.timeWindow,
+                limited: false
+            };
+        };
+        if(Date.now() - player.timestamp >= this.timeWindow) {
+            this.ips.delete(ip);
+            return this.getRatelimitData(ip);
+        }
+        return {
+            limited: player.requests++ >= this.maxRequests,
+            remaining: this.maxRequests - player.requests,
+            reset: player.timestamp + this.timeWindow - Date.now()
+        };
+    }
+}
+
+module.exports = Ratelimiter;
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require(`express`);
+const http = require(`http`);
+const parser = require(`body-parser`);
+const TelegramAPI = require(`node-telegram-bot-api`);
+const Ratelimiter = require(`./Ratelimiter`);
+const app = express();
+app.disable(`x-powered-by`);
+app.set(`trust proxy`, true);
+app.use(parser.json());
+
+global.server = {};
+server.cfg = require(`./config.json`);
+server.ratelimiter = new Ratelimiter();
+server.bot = new TelegramAPI(server.cfg.token, { polling: true });
+
+server.app = http.createServer(app).listen(server.cfg.port, () => {
+    console.log(`Server listening on port ${server.cfg.port}`);
+});
+
+app.post(`/:user`, (req, res) => {
+    if(server.ratelimiter.ratelimitResponse(req, res)) return;
+    const username = req.params.user.toLowerCase();
+    const user = server.cfg.users.find((u) => u.id == username || u.aliases.includes(username));
+    const message = req.body.message;
+
+    if(!user) return res.status(404).send({ message: `404: User not found!` });
+    if(user.auth.active && user.auth.key != req.headers.authorization) return res.status(403).send({ message: `403: You need to enter the key to send messages to this user!` });
+    if(!message) return res.status(400).send({ message: `400: No message provided!` });
+    server.bot.sendMessage(user.conversation, message);
+    res.send({ message: `Message successfully sent to ${user.id}!` });
+});
+
+app.use((req, res, next) => {
+    res.status(404).send({ message: `404: Not found` });
+});
